32e6392aeb116a70faf9b6e3c80d818829e09d8d
pfeitor <pedro_feitor@hotmail.com>
2017-05-12 10:20:10 +0100

    Mapa deficiente, com Firebase

---
+++ MyApplication/app/src/main/java/com/example/pedrofeitor/mypoint/Item.java
@@ -0,0 +1,24 @@
+package com.example.pedrofeitor.mypoint;
+
+/**
+ * Created by Pedro Feitor on 11/05/2017.
+ */
+
+public class Item {
+
+    private String title;
+
+    public Item() {}
+
+    public Item(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
